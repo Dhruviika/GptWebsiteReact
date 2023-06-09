8be383819c311f576626379865170079a60e939c
Dhruviika <E21CSEU0292@bennett.edu.in>
2023-06-09 20:50:54 +0530

    brands and what chatgpt is added

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,8 +1,27 @@
 import React from "react";
 import "./brand.css";
+import { google, dropbox, shopify, slack, atlassian } from "./index";
 
 const Brand = () => {
-  return <div>Brand</div>;
+  return (
+    <div className="brand section_padding">
+      <div>
+        <img src={google} />
+      </div>
+      <div>
+        <img src={dropbox} />
+      </div>
+      <div>
+        <img src={shopify} />
+      </div>
+      <div>
+        <img src={slack} />
+      </div>
+      <div>
+        <img src={atlassian} />
+      </div>
+    </div>
+  );
 };
 
 export default Brand;
